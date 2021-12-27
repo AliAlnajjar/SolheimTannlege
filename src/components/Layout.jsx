c6c49618feb34a9875c772ff15763637c1df20a6
Ali Alnajjar <ali_alnajjar@live.com>
2021-12-27 16:19:52 +0100

    crossOrigin

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -12,7 +12,7 @@ export default function Layout({ children }) {
         <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
 
         <link rel="preconnect" href="https://fonts.googleapis.com" /> 
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> 
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900" rel="stylesheet"/>
       </head>
       <div className={styles.container}>

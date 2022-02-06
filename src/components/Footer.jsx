7edc7abcf4cbc9f5492a4e168c8c3848a13d8f5b
Ali Alnajjar <ali_alnajjar@live.com>
2022-02-06 13:43:13 +0100

    almost there2 :D

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,24 @@
 import styles from '../styles/Layout.module.css';
-
+import Link from 'next/link';
+import FacebookIcon from '@mui/icons-material/Facebook';
 export default function Footer() {
-  return <footer className={styles.footer}>footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <ul>
+        <li className={styles.logo}>
+          <h1>SolheimTannklinikk</h1>
+        </li>
+        <li>
+          Åpningstider
+          <p>mand-fre: 09:00 - 19:00 </p>
+          <p>lørdag : 09:00 - 19:00 </p>
+          <p>søndag : 09:00 - 19:00 </p>
+        </li>
+        <li>
+          Adresse:
+          <p>Fjøsangervien 39</p>
+        </li>
+      </ul>
+    </footer>
+  );
 }

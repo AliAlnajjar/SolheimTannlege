b03563b8e94a0d0a6252a5ac2816cea30b644388
Ali ibrahim alnajjar <ali.ibrahim.alnajjar@dnb.no>
2022-02-23 16:28:08 +0100

    fix the link error

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from "./Hero.module.scss";
-import Link from "next/Link";
+import Link from "next/link";
 export default function Hero() {
   return (
     <div className={styles.hero}>
